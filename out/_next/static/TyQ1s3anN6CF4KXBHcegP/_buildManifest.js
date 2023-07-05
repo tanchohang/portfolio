ebed74c2b767f0c13c8daac5aa6dc714c654b336
tanchohang <tanchohang@gmail.com>
2023-07-05 10:35:39 +0100

    build: new fix build

---
+++ out/_next/static/TyQ1s3anN6CF4KXBHcegP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/528-d59f2c2633d159b1.js","static/css/1a2b64110b6b3f1d.css","static/chunks/pages/index-b827c40071e5cead.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- pages/index.js
+++ pages/index.js
@@ -23,46 +23,63 @@ export default function Home() {
       description: `The chat application is built on React on the client side, NestJS and MongoDB on the backend side. It has features like
       authentication, group chat, video and audio call. It uses WebRTC and socket.io to facilitates the communication.`,
       tags: ['Javascript', 'React', 'WebRTC', 'Socket.io', 'NestJS', 'MongoDB', 'TailwindCSS'],
+      links: [
+        { type: 'github', url: 'https://github.com/tanchohang/keepup-client' },
+        { type: 'github', url: 'https://github.com/tanchohang/keepup-api' },
+        { type: 'web', url: 'https://keepup.tanchohang.dev' },
+      ],
     },
     {
       title: 'Skyvault: A Photos Storage and Sharing App',
       description: `Skyvault lets users upload their photos and share it. Specifically built as a mini mock of cloud storage for use in personal
       projects. The backend is built on NodeJS and NextJS is used for the frontend.`,
       tags: ['Javascript', 'Next.js', 'NestJS', 'MongoDB', 'TailwindCSS'],
+      links: [
+        { type: 'github', url: 'https://github.com/tanchohang/skyvault-api' },
+        { type: 'github', url: 'https://github.com/tanchohang/skyvault-client' },
+        { type: 'web', url: 'https://skyvault.tanchohang.dev' },
+      ],
     },
-    // {
-    //   title: '',
-    //   description: '',
-    //   tags: [],
-    // },
+    {
+      title: 'Skyvault: A Photos Storage and Sharing App',
+      description: `Skyvault lets users upload their photos and share it. Specifically built as a mini mock of cloud storage for use in personal
+      projects. The backend is built on NodeJS and NextJS is used for the frontend.`,
+      tags: ['Javascript', 'Next.js', 'NestJS', 'MongoDB', 'TailwindCSS'],
+      links: [{ type: 'github', url: 'https://github.com/' }],
+    },
   ];
   const projects = [
     {
       title: 'SmartHomeTech',
       description: 'Smart Home Website is a website that I worked on in my placement year.',
       tags: ['Python', 'Django', 'React'],
+      links: [{ type: 'github', url: 'https://github.com/' }],
     },
 
     {
       title: 'Automatic-Impression-Generation-From-Medical-Imaging-Report',
       description: 'Project was for my desertation',
       tags: ['Python', 'Django', 'React', 'MAchine Learning'],
+      links: [{ type: 'github', url: 'https://github.com/' }],
     },
 
     {
       title: 'Portfolio',
       description: 'This is the portfolio your are currently seeing',
       tags: ['Javascript', 'NextJs'],
+      links: [{ type: 'github', url: 'https://github.com/' }],
     },
     {
       title: 'Langtang',
       description: 'A Tailwind based React Component Library',
       tags: ['Javascript', 'Tailwind', 'Reactjs', 'Storybook'],
+      links: [{ type: 'github', url: 'https://github.com/' }],
     },
     {
       title: 'Restaurant Management system',
       description: 'This system was built as my final year project for my undergraduate project. Built on Firebase and Angular',
       tags: ['Javascript', 'Angular', 'Bootstrap', 'Firebase'],
+      links: [{ type: 'github', url: 'https://github.com/' }],
     },
   ];
 
@@ -107,7 +124,7 @@ export default function Home() {
           <Loading />
         </div>
       ) : (
-        <main className="min-h-screen font-mono md:grid md:grid-cols-2">
+        <main className="min-h-screen font-mono text-base md:grid md:grid-cols-2">
           <section className="h-[100vh] w-[50vw] bg-mylightblue fixed hidden md:block">
             <Intro />
           </section>
@@ -124,34 +141,45 @@ export default function Home() {
                 <Scroll />
               </div>
             </section>
-
-            <section className="h-[100vh] sticky top-0 bg-mypallate1 text-mylight font-sans" ref={rootObserver}>
-              <h2 className=" text-2xl text-center p-10">Projects I&apos;ve Built</h2>
-              <div className="flex flex-col items-center gap-5 h-[80vh]">
+            {/* PROJECTS */}
+            <section className=" h-[150vh] sticky top-0 pb-20 bg-mypallate1 text-mylight font-sans" ref={rootObserver}>
+              <h2 className=" text-2xl text-center p-5 ">
+                <span className="">Projects I&apos;ve Built</span>
+              </h2>
+              <div className="flex flex-col   items-center gap-5">
                 {/* Card1 */}
                 {featured.map((project, i) => {
                   return (
                     <div className={isVisible1 ? 'visible animate-slideRight' : 'visible'} key={i}>
-                      <div className="group bg-cover bg-mynavy bg-blend-overlay text-mylight w-[90vw] md:h-[30vh] md:w-[40vw] lg:w-[35vw]  opacity-90 shadow-rb flex flex-col justify-center cursor-pointer rounded-sm overflow-hidden">
+                      <div className="group bg-cover bg-mynavy bg-blend-overlay text-mylight max-h-[30vh] min-h-[20vh] w-[90vw] md:w-[40vw] lg:w-[35vw]  opacity-90 shadow-rb flex flex-col justify-center cursor-pointer rounded-sm overflow-auto">
                         <div className=" p-5 ">
-                          <h6 className=" text-xs font-light text-teal-500">Featured Project</h6>
-                          <h5 className=" font-sans font-normal text-mylight group-hover:text-teal-500">{project.title}</h5>
-                          <div className="text-sm font-sans text-gray-300 mt-1">
-                            <p className="text-justify">{project.description}</p>
-                            <p className="mt-2 text-[.55rem] text-gray-500 flex gap-1">
+                          <h6 className=" text-xs  font-light text-teal-500">Featured Project</h6>
+                          <h5 className=" font-sans md:text-xl lg:text-2xl font-normal text-mylight group-hover:text-teal-500">{project.title}</h5>
+                          <div className="text-sm md:text-lg lg:text-xl font-sans text-gray-300 mt-1">
+                            <p className="">{project.description}</p>
+                            <p className="mt-2 text-[.55rem] md:text-sm lg:text-lg text-gray-500 flex flex-wrap gap-2">
                               {project.tags.map((tag) => (
                                 <code key={tag}>{tag}</code>
                               ))}
                             </p>
-                            <div className="flex">
-                              <div className=" w-5 h-5">
-                                <a href="https://github.com/tanchohang/keepup-api">
-                                  <Image width={20} height={20} src="/github-logo.svg" alt="https://github.com/tanchohang/keepup-api" />
-                                </a>
-                              </div>
-                              <div className=" w-5 h-5">
-                                <a href="https://github.com/tanchohang/keepup-client"></a>
-                              </div>
+                            <div className="flex gap-2">
+                              {project.links.map((link) => {
+                                return (
+                                  <div key={link.url}>
+                                    {link.type === 'github' && (
+                                      <a href={link.url} className=" bg-white block rounded-sm">
+                                        <Image width={20} height={20} src="/github-logo.svg" alt={link.url} />
+                                      </a>
+                                    )}
+
+                                    {link.type === 'web' && (
+                                      <a href={link.url} className="bg-white block">
+                                        <Image width={20} height={20} src="/open-in-new-window.svg" alt={link.url} />
+                                      </a>
+                                    )}
+                                  </div>
+                                );
+                              })}
                             </div>
                           </div>
                         </div>
@@ -159,24 +187,23 @@ export default function Home() {
                     </div>
                   );
                 })}
-
                 {/* End of Card1 */}
               </div>
             </section>
 
-            <section className="h-[100vh] md:w-[100%] sticky top-0 bg-mypallate4 text-mylight font-sans overflow-y-auto">
-              <h2 className="text-2xl text-center p-10">Other Projects I&apos;ve worked on</h2>
-              <div className="flex flex-wrap justify-center gap-8">
+            <section className=" h-[100vh] sticky top-0 bg-mypallate4 text-mylight font-sans">
+              <h2 className="text-2xl text-center p-5">Other Projects I&apos;ve worked on</h2>
+              <div className=" flex flex-wrap justify-center gap-5">
                 {projects.map((p, i) => {
                   return (
                     <div
-                      className="group bg-mynavy w-[70%] lg:w-[45%] flex flex-col gap-5 px-5 rounded-sm shadow-black shadow-xl hover:-translate-y-3 transition-transform ease-in-out duration-500 delay-75 cursor-pointer p-10"
+                      className="group bg-mynavy min-h-[15vh] max-h-[20vh] w-[45%] flex flex-col gap-1 px-5 rounded-sm shadow-black shadow-xl hover:-translate-y-3 transition-transform ease-in-out duration-500 delay-75 cursor-pointer py-2 overflow-auto"
                       key={i}
                     >
                       <h2 className=" text-base group-hover:text-teal-500 font-semibold">{p.title}</h2>
                       <p className="  text-sm">{p.description}</p>
                       <footer className=" mt-2 text-xs text-gray-500 group-hover:animate-bounce">
-                        <code className="flex gap-1">
+                        <code className="flex flex-wrap gap-1 md:gap-2">
                           {p.tags.map((tag) => (
                             <span key={tag}>{tag}</span>
                           ))}
@@ -187,42 +214,41 @@ export default function Home() {
                 })}
               </div>
               {/* {isShowMore && (
-                <div className="flex flex-wrap justify-center gap-8 mt-8">
-                  {Array.from(Array(2)).map((_, i) => {
-                    return (
-                      <div
-                        className="group bg-mynavy w-[70%] lg:w-[45%] flex flex-col gap-5 px-5 rounded-sm shadow-black shadow-xl hover:-translate-y-3 transition-transform ease-in-out duration-500 delay-75 cursor-pointer p-10"
-                        key={i}
-                      >
-                        <h2 className=" text-base group-hover:text-teal-500 font-semibold">Apple Music Embeddable Web Player Widget</h2>
-                        <p className="  text-sm">
-                          Building a custom multisite compatible WordPress plugin to build global search with Algolia. Embeddable web player widget
-                          for Apple Music that lets users log in and listen to full song playback in the browser leveraging
-                        </p>
-                        <footer className=" mt-2 text-xs text-gray-500 group-hover:animate-bounce">
-                          <code> PHP Laravel Angular</code>
-                        </footer>
-                      </div>
-                    );
-                  })}
-                </div>
-              )} */}
-
+                  <div className="flex flex-wrap justify-center gap-8 mt-8">
+                    {Array.from(Array(2)).map((_, i) => {
+                      return (
+                        <div
+                          className="group bg-mynavy w-[70%] lg:w-[45%] flex flex-col gap-5 px-5 rounded-sm shadow-black shadow-xl hover:-translate-y-3 transition-transform ease-in-out duration-500 delay-75 cursor-pointer p-10"
+                          key={i}
+                        >
+                          <h2 className=" text-base group-hover:text-teal-500 font-semibold">Apple Music Embeddable Web Player Widget</h2>
+                          <p className="  text-sm">
+                            Building a custom multisite compatible WordPress plugin to build global search with Algolia. Embeddable web player widget
+                            for Apple Music that lets users log in and listen to full song playback in the browser leveraging
+                          </p>
+                          <footer className=" mt-2 text-xs text-gray-500 group-hover:animate-bounce">
+                            <code> PHP Laravel Angular</code>
+                          </footer>
+                        </div>
+                      );
+                    })}
+                  </div>
+                )} */}
               {/* <div className="my-10 text-center">
-                {isShowMore ? (
-                  <button className="outline rounded-sm py-3 px-10 hover:bg-mydarkgreen hover:text-mylight" onClick={() => setIsShowMore(false)}>
-                    Show less
-                  </button>
-                ) : (
-                  <button className="outline rounded-sm py-3 px-10 hover:bg-mydarkgreen hover:text-mylight" onClick={() => setIsShowMore(true)}>
-                    Show more
-                  </button>
-                )}
-              </div> */}
+                  {isShowMore ? (
+                    <button className="outline rounded-sm py-3 px-10 hover:bg-mydarkgreen hover:text-mylight" onClick={() => setIsShowMore(false)}>
+                      Show less
+                    </button>
+                  ) : (
+                    <button className="outline rounded-sm py-3 px-10 hover:bg-mydarkgreen hover:text-mylight" onClick={() => setIsShowMore(true)}>
+                      Show more
+                    </button>
+                  )}
+                </div> */}
             </section>
-            <section className="h-[70vh] sticky shadow-white shadow-2xl bg-mypallate1 text-mylight">
+            <section className="h-[70vh] sticky  shadow-white shadow-2xl bg-mypallate1 text-mylight">
               <div className="flex gap-2 justify-center items-center h-full">
-                <h4 className=" text-lg bg-red-400">Thank You For Visiting!</h4>
+                <h4 className=" text-lg bg-red-400 p-2">Thank You For Visiting!</h4>
                 <Image src="/consulting.png" alt="emoji" width={40} height={40} />
               </div>
             </section>
